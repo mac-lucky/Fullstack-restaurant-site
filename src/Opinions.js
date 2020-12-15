8d721b222568f3122da05a0cba9f28ba40b58472
Maciej Kędziora <maciejkedziora98@gmail.com>
2020-12-15 19:36:03 +0100

    add google maps reviews

--- src/Opinions.js
+++ src/Opinions.js
@@ -1,27 +1,14 @@
 import React, { Component } from "react";
-import "./Aboutus.css";
-import onas from "./images/onas.png";
+import "./Opinions.css";
+import googlePlaces from "google-maps-reviews";
+
 
 class Opinions extends Component {
   render() {
     return (
       <>
-        <div className="width">
-          <div className="separator">OPINIE NASZYCH GOŚCI</div>
-          <div className="flex">
-            <img src={onas}></img>
-            <div className="text">
-              <h2>Domowa kuchnia i niepowtarzalny smak</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptates pariatur magnam numquam. Provident nobis
-                reprehenderit saepe corporis repellendus soluta molestiae
-                aperiam aut itaque iste, cupiditate, dolores ad ratione
-                blanditiis commodi.
-              </p>
-            </div>
-          </div>
-        </div>
+        {googlePlaces}
+        <div id="google-reviews"></div>
       </>
     );
   }
