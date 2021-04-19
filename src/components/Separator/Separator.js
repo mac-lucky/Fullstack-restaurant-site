46455d1a499ecd7177d3bfe152bf2f0b79988f62
Maciej Kędziora <maciejkedziora98@gmail.com>
2021-04-19 19:33:23 +0200

    delete env port

--- src/components/Separator/Separator.js
+++ src/components/Separator/Separator.js
@@ -2,16 +2,17 @@ import "./Separator.css";
 import React from "react";
 import { MDBContainer, MDBIcon } from "mdbreact";
 
-const Separator = () => { 
-    return (
-      <>
-        <MDBContainer fluid className="space">
-          <MDBIcon className="icon" icon="quote-left" />
-          <h1>Można nie jeść w ogóle, ale nie można jeść źle.</h1>
-          <MDBIcon className="icon down" icon="quote-right" />
-        </MDBContainer>
-      </>
-    );
-  }
+const Separator = () => {
+  return (
+    <>
+      <MDBContainer fluid className="space">
+        <MDBIcon className="icon" icon="quote-left" />
+        <h1>Można nie jeść w ogóle, ale nie można jeść źle.</h1>
+        <MDBIcon className="icon down" icon="quote-right" />
+      </MDBContainer>
+      <div></div>
+    </>
+  );
+};
 
 export default Separator;
